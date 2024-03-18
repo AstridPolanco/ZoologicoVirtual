a8b9841663988a19f1d6a6fc788efa5e54462004
mlyyppol <apenap1@miumg.edu.gt>
2024-03-17 22:44:01 -0600

    zoologico

---
+++ Zoologico/ClasesAnimales.cs
@@ -0,0 +1,666 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoologico
+{
+    class Leon
+    {
+        private string nombre {  get; set; }
+
+        private int altura { get; set; }
+
+        private int peso { get; set; }
+
+        private string sexo { get; set; }
+
+        private int edad {  get; set; }
+
+        private string especie = "Leon";
+
+        public Leon(string nombre, int altura, int peso, string sexo, int edad, string especie)
+        {
+            this.nombre = nombre;
+            this.altura = altura;
+            this.peso = peso;
+            this.sexo = sexo;
+            this.edad = edad;
+            this.especie = especie;
+        }
+        public Leon(string nombre, string sexo)
+        {
+            this.nombre = nombre;
+            this.sexo = sexo;
+        }
+
+        public void alimentar()
+        {
+            Console.WriteLine("Come carne");
+        }
+
+        public void ruido()
+        {
+            Console.WriteLine("GRRRRRR");
+        }
+        public override string ToString()
+        {
+            return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+        }
+    }
+}
+
+class Elefante
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie="Elefante";
+
+    public Elefante(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Elefante(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come hierbas");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("FUUUUU");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+
+class Jirafa
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Jirafa";
+
+    public Jirafa(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Jirafa(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come hojas");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("UUUUUH");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+class Tigre
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Tigre";
+
+    public Tigre(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Tigre(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come carne");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("GRRRRRR");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+
+class Mono
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Mono";
+
+    public Mono(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Mono(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come frutos");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("UHUHUHUH");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+
+class Oso
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Oso";
+
+    public Oso(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Oso(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come peces");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("RAAAAAW");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+
+class Cocodrilo
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Cocodrilo";
+
+    public Cocodrilo(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Cocodrilo(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come peces");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("GRRRRR");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+class Serpiente
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Serpiente";
+
+    public Serpiente(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Serpiente(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come ratones");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("SSSSSSS");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+
+class Pinguino
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Pinguino";
+
+    public Pinguino(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Pinguino(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come peces");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("RRRGGG");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+
+class Lobo
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Lobo";
+
+    public Lobo(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Lobo(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come carne");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("AUUUUUU");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+
+class Cebra
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Cebra";
+
+    public Cebra(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Cebra(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come hierbas");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("YIIIIIP");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+
+class Rinoceronte
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Rinoceronte";
+
+    public Rinoceronte(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Rinoceronte(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come ramas");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("RUUUUUUU");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+
+class Panda
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Panda";
+
+    public Panda(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Panda(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come bambú");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("GRUGRUGRU");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+
+class PavoReal
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Pavo Real";
+
+    public PavoReal(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public PavoReal(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come semillas");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("AHAAAAAA");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
+
+class Tortuga
+{
+    private string nombre { get; set; }
+
+    private int altura { get; set; }
+
+    private int peso { get; set; }
+
+    private string sexo { get; set; }
+
+    private int edad { get; set; }
+
+    private string especie = "Tortuga";
+
+    public Tortuga(string nombre, int altura, int peso, string sexo, int edad, string especie)
+    {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.especie = especie;
+    }
+    public Tortuga(string nombre, string sexo)
+    {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void alimentar()
+    {
+        Console.WriteLine("Come algas marinas");
+    }
+
+    public void ruido()
+    {
+        Console.WriteLine("UHHHH");
+    }
+    public override string ToString()
+    {
+        return "Nombre: " + "\n" + this.nombre + "\n" + "Especie: " + this.especie;
+    }
+}
---
+++ Zoologico/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoologico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            bool abandonar = false;
+
+            do
+            {
+                Console.WriteLine("Bienvenidos al Zoologico virtual");
+                Console.WriteLine("-------------------------------------------------------");
+                Console.WriteLine("0. Abandonar");
+                Console.WriteLine("1. Visitar al Leon");
+                Console.WriteLine("2. Visitar al Elefante");
+                Console.WriteLine("3. Visitar a la Jirafa");
+                Console.WriteLine("4. Visitar al tigre");
+                Console.WriteLine("5. Visitar al Mono");
+                Console.WriteLine("6. Visitar al Oso");
+                Console.WriteLine("7. Visitar al Cocodrilo");
+                Console.WriteLine("8. Visitar a la Serpiente");
+                Console.WriteLine("9. Visitar al Pinguino");
+                Console.WriteLine("10. Visitar al Lobo");
+                Console.WriteLine("11. Visitar a la Cebra");
+                Console.WriteLine("12. Visitar al rinoceronte");
+                Console.WriteLine("13. Visitar al Panda");
+                Console.WriteLine("14. Visitar al Pavo Real");
+                Console.WriteLine("15. Visitar a la Tortuga");
+                Console.Write("Orientador: ¿Que deseas hacer?");
+                int opcion = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("-----------------------------------------------------------");
+
+                switch (opcion)
+                {
+
+                    case 0: abandonar = true; break;
+                    case 1: visitarLeon(); break;
+                    case 2: visitarElefante(); break;
+                    case 3: visitarJirafa(); break;
+                    case 4: visitarTigre(); break;
+                    case 5: visitarMono(); break;
+                    case 6: visitarOso(); break;
+                    case 7: visitarCocodrilo(); break;
+                    case 8: visitarSerpiente(); break;
+                    case 9: visitarPinguino(); break;
+                    case 10: visitarLobo(); break;
+                    case 11: visitarCebra(); break;
+                    case 12: visitarRinoceronte(); break;
+                    case 13: visitarPanda(); break;
+                    case 14: visitarPavoReal(); break;
+                    case 15: visitarTortuga(); break;
+                    default: Console.WriteLine("La opcion no está dentro de los parametros"); break;
+
+
+                }
+                Console.ReadKey();
+                Console.Clear();
+
+            } while (!abandonar);
+
+        }
+
+        public static void visitarLeon()
+        {
+            Leon leon = new Leon("Milles", "Macho");
+            Console.WriteLine("En la jaula esta {0} ", leon);
+            leon.alimentar();
+            leon.ruido();
+        }
+
+        public static void visitarElefante()
+        {
+            Elefante elefante = new Elefante("Carl", "Macho");
+            Console.WriteLine("En la jaula esta {0} ", elefante);
+            elefante.alimentar();
+            elefante.ruido();
+        }
+
+        public static void visitarJirafa()
+        {
+            Jirafa jirafa = new Jirafa("Mila", "Hembra");
+            Console.WriteLine("En la jaula esta {0} ", jirafa);
+            jirafa.alimentar();
+            jirafa.ruido();
+        }
+
+        public static void visitarTigre()
+        {
+            Tigre tigre=new Tigre("Leonard","Macho");
+            Console.WriteLine("En la jaula esta {0} ", tigre);
+            tigre.alimentar();
+            tigre.ruido();
+        }
+
+        public static void visitarMono()
+        {
+            Mono mono = new Mono("Jules", "Hembra");
+            Console.WriteLine("En la jaula esta {0} ", mono);
+            mono.alimentar();
+            mono.ruido();
+        }
+
+        public static void visitarOso()
+        {
+            Oso oso = new Oso ("Canela", "Macho");
+            Console.WriteLine("En la jaula esta {0} ", oso);
+            oso.alimentar();
+            oso.ruido();
+        }
+
+        public static void visitarCocodrilo()
+        {
+        Cocodrilo cocodrilo=new Cocodrilo("Carl", "Macho");
+            Console.WriteLine("En la jaula esta {0} ", cocodrilo);
+            cocodrilo.alimentar();
+            cocodrilo.ruido();
+        }
+
+        public static void visitarSerpiente()
+        {
+            Serpiente serpiente = new Serpiente("Iris", "Hembra");
+            Console.WriteLine("En la jaula esta {0} ", serpiente);
+            serpiente.alimentar();
+            serpiente.ruido();
+        }
+
+        public static void visitarPinguino()
+        {
+        Pinguino pinguino= new Pinguino("Jack","Macho");
+            Console.WriteLine("En la jaula esta {0} ", pinguino);
+            pinguino.alimentar();
+            pinguino.ruido();
+        }
+
+        public static void visitarLobo()
+        {
+            Lobo lobo= new Lobo("Jacobs", "Macho");
+            Console.WriteLine("En la jaula esta {0} ", lobo);
+            lobo.alimentar();
+            lobo.ruido();
+        }
+
+        public static void visitarCebra()
+        {
+            Cebra cebra = new Cebra("Susie", "Hembra");
+            Console.WriteLine("En la jaula esta {0} ", cebra);
+            cebra.alimentar();
+            cebra.ruido();
+        }
+
+        public static void visitarRinoceronte()
+        {
+        Rinoceronte rinoceronte = new Rinoceronte("Rudolf", "Macho");
+            Console.WriteLine("En la jaula esta {0} ", rinoceronte);
+            rinoceronte.alimentar();
+            rinoceronte.ruido();
+        }
+
+        public static void visitarPanda()
+        {
+            Panda panda= new Panda("Bepo","Macho");
+            Console.WriteLine("En la jaula esta {0} ", panda);
+            panda.alimentar();
+            panda.ruido();
+        }
+        public static void visitarPavoReal()
+        {
+            PavoReal pavoReal = new PavoReal("Elena", "Hembra");
+            Console.WriteLine("En la jaula esta {0} ", pavoReal);
+            pavoReal.alimentar();
+            pavoReal.ruido();
+        }
+        public static void visitarTortuga()
+        {
+         Tortuga tortuga = new Tortuga("Luffy", "Macho");
+            Console.WriteLine("En la jaula esta {0} ", tortuga);
+            tortuga.alimentar();
+            tortuga.ruido();
+        
+        }
+
+    }
+}
